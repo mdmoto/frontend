0c2bd6ef76f258a00f6bfccc64e98501e66f336f
mdmoto <61302600+mdmoto@users.noreply.github.com>
2025-10-29 13:05:51 +0900

    配置管理端 API 指向 AWS EC2 服务器

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn",
+    common: "http://54.255.179.211:8891",
+    buyer: "http://54.255.179.211:8888",
+    seller: "http://54.255.179.211:8889",
+    manager: "http://54.255.179.211:8890",
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "http://54.255.179.211:8891",
+    buyer: "http://54.255.179.211:8888",
+    seller: "http://54.255.179.211:8889",
+    manager: "http://54.255.179.211:8890"
   },
   /**
    * @description // 跳转买家端地址 pc端
